de0a8b718333650f691a7e0d52b1aafce4c4528a
Eduardo Sanchez <esanchez@student.42lausanne.ch>
2023-05-16 16:40:39 +0200

    update delete request

--- srcs/deleteRequest.cpp
+++ srcs/deleteRequest.cpp
@@ -4,13 +4,10 @@ bool isValidPath(const std::string& path)
 {
     if (path.find("..") != std::string::npos)
         return false;
-
     std::string invalidChars = "*:?|<>\"";
-    for (std::size_t i = 0; i < path.length(); ++i)
-    {
-        if (invalidChars.find(path[i]) != std::string::npos)
-            return false;
-    }
+    if (path.find_first_of(invalidChars) != std::string::npos)
+        return false;
+
     return true;
 }
 
@@ -19,12 +16,20 @@ void Webserv::deleteRequest(s_request *requestData, int fd)
     std::string fullPath = _rootpath + requestData->addr;
     if (!isValidPath(requestData->addr))
         code_error(fd, 404);
-    // else if (std::remove(fullPath.c_str()) == 0)
-    else if (1)
-    {
-        std::string response = "HTTP/1.1 202 Accepted\r\nContent-Type: text/plain\r\nContent-Length: 0\r\nConnection: close\r\n\r\n";
-        send(_client_sockfd[fd], response.c_str(), response.size(), 0);
-    }
     else
-        code_error(fd, 404);
+    {
+        std::ifstream file(fullPath);
+        if (!file)
+            code_error(fd, 404);
+        else
+        {
+            file.close();
+            if (std::remove(fullPath.c_str()) == 0)
+            {
+                std::string response = "HTTP/1.1 202 Accepted\r\nContent-Type: text/plain\r\nContent-Length: 0\r\nConnection: close\r\n\r\n";
+                send(_client_sockfd[fd], response.c_str(), response.size(), 0);
+            } else
+                code_error(fd, 500);
+        }
+    }
 }
