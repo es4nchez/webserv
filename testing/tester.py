086f3ef18b09fe2bbe4bc27fce10f53f13496ce2
Eduardo Sanchez <esanchez@student.42lausanne.ch>
2023-05-16 17:29:12 +0200

    update testing

--- testing/tester.py
+++ testing/tester.py
@@ -2,18 +2,19 @@ import unittest
 import requests
 
 get_endpoint = "/42lWatch.html"
-
 get_cgi = "/cgi_script.py"
-
-post_endpoint = "/postex.html"
+post_endpoint = "/post.py"
 
 class TestWebserver(unittest.TestCase):
-
     BASE_URL = 'http://localhost:8080'
 
     def test_get(self):
         response = requests.get(f'{self.BASE_URL}')
         self.assertEqual(response.status_code, 200)
+
+    def test_404(self):
+        response = requests.get(f'{self.BASE_URL}/notfoubddd')
+        self.assertEqual(response.status_code, 404)
 
     def test_get2(self):    
         response = requests.get(f'{self.BASE_URL}{get_endpoint}')
@@ -23,16 +24,20 @@ class TestWebserver(unittest.TestCase):
         response = requests.get(f'{self.BASE_URL}{get_cgi}')
         self.assertEqual(response.status_code, 200)
 
-
     def test_post(self):
         data = {"name": "eduardo"}
-        response = requests.post(f'{self.BASE_URL}/post.py', json=data)
+        response = requests.post(f'{self.BASE_URL}{post_endpoint}', json=data)
         self.assertEqual(response.status_code, 200)
 
-    # def test_delete(self):
-    #     response = requests.delete(f'{self.BASE_URL}/path')
-    #     self.assertEqual(response.status_code, 200)
+    def test_delete(self):
+        response = requests.delete(f'{self.BASE_URL}/upload/test.txt')
+        self.assertEqual(response.status_code, 202)
+
+    def test_bad_delete(self):
+        response = requests.delete(f'{self.BASE_URL}/upload/dontexist.txt')
+        self.assertEqual(response.status_code, 404)
 
 
 if __name__ == '__main__':
     unittest.main()
+
