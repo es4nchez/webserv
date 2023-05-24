6641690c6633dd508ccf1b2fa5cebc28061104af
Eduardo Sanchez <esanchez@student.42lausanne.ch>
2023-05-24 14:46:20 +0200

    fix CGI env in execve

--- srcs/cgi.cpp
+++ srcs/cgi.cpp
@@ -13,14 +13,14 @@ CGI::~CGI()
 
 void CGI::handle_cgi_request(int sockfd, const std::string& cgi_path, char **_wenvp)
 {
-    (void) _wenvp;
+
     int pipefd[2];
     if (pipe(pipefd) == -1) {
         std::cerr << "Error creating pipe: " << strerror(errno) << std::endl;
         return;
     }
-    pid_t pid = fork();
 
+    pid_t pid = fork();
     if (pid == -1) {
         std::cerr << "Error forking process: " << strerror(errno) << std::endl;
         return;
@@ -33,7 +33,7 @@ void CGI::handle_cgi_request(int sockfd, const std::string& cgi_path, char **_we
         close(pipefd[1]);
 
         char* args[] = { const_cast<char*>(PYTHON), const_cast<char*>(cgi_path.c_str()), NULL};
-        execve(PYTHON, args, NULL);
+        execve(PYTHON, args, _wenvp);
         std::cerr << "Error executing CGI program: " << strerror(errno) << std::endl;
         exit(EXIT_FAILURE);
     }
@@ -45,13 +45,26 @@ void CGI::handle_cgi_request(int sockfd, const std::string& cgi_path, char **_we
     char buffer[1024];
     int bytes_read;
 
+    std::time_t start_time = std::time(nullptr);
+    const int max_execution_time = 5;
+
     while ((bytes_read = read(pipefd[0], buffer, sizeof(buffer))) > 0) {
+        std::time_t current_time = std::time(nullptr);
+        if (current_time - start_time > max_execution_time) {
+            std::cerr << "CGI execution time exceeded the limit. Terminating process." << std::endl;
+            kill(pid, SIGKILL);
+            break;
+        }
         write(sockfd, buffer, bytes_read);
     }
+
     if (bytes_read == -1)
         std::cerr << "Error reading CGI output: " << strerror(errno) << std::endl;
     close(pipefd[0]);
 
+    // Wait for the child process to avoid leaving zombies
+    int pid_status;
+    waitpid(pid, &pid_status, WNOHANG);
 }
 
 #include <algorithm>
